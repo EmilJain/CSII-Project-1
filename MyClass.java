73092b3ba49e19be32a366408733eaab6af7a929
27kramer <27kramer@friendsseminary.org>
2025-03-10 14:58:56 -0400

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+public class MyClass {
+    public static void main(String args[]) {
+        Card[] cards = Search.makeDeck();
+
+        // Create a deck of cards
+        Card[] deck = Card.makeDeck();
+
+        System.out.println("shuffled deck:");
+        Deck shuffleDeck = new Deck();
+        shuffleDeck.shuffle();
+        shuffleDeck.print();
+
+     
+
+
+
+
+
+
+
+
+
+
+    }
+}
