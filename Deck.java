d4b74cbc55b4a5266749321dcbceeaa598fa4e23
27kramer <27kramer@friendsseminary.org>
2025-03-12 12:03:51 -0400

    Update Deck.java to complete past methods

--- Deck.java
+++ Deck.java
@@ -1,18 +1,15 @@
 import java.util.Random;
 
 public class Deck {
+    // This is a class variable so we don't have to create
+    // a new Random object every time we call randomInt.
+    private static Random random = new Random();
 
     private Card[] cards;
-    private static final Random random = new Random();
 
-    public Deck(int n) {
-        this.cards = new Card[n];
-    }
-
-    public Card[] getCards() {
-        return this.cards;
-    }
-
+    /**
+     * Constructs a standard deck of 52 cards.
+     */
     public Deck() {
         this.cards = new Card[52];
         int index = 0;
@@ -24,93 +21,173 @@ public class Deck {
         }
     }
 
+    /**
+     * Constructs a deck of n cards (all null).
+     */
+    public Deck(int n) {
+
+        this.cards = new Card[n];
+    }
+
+    /**
+     * Gets the internal cards array.
+     */
+    public Card[] getCards() {
+        return this.cards;
+    }
+
+    /**
+     * Displays each of the cards in the deck.
+     */
     public void print() {
         for (Card card : this.cards) {
             System.out.println(card);
         }
     }
 
-    public String toString(){
-        String result = "";
-        for(Card card:this.cards){
-            result += card.toString() + "";
+    /**
+     * Returns a string representation of the deck.
+     */
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < this.cards.length; i++) {
+            str.append(this.cards[i]);
+            str.append('\n');
         }
-        return "TODO";
+        return str.toString();
     }
 
-    private static int randomInt(int low, int high){
-        return random.nextInt(high - low) + low;
+    /**
+     * Randomly permutes the array of cards.
+     */
+    public void shuffle() {
+        for (int i = 0; i < this.cards.length; i++) {
+            int rand = randomInt(i, this.cards.length - 1);
+            swapCards(i, rand);
+        }
     }
 
+    /**
+     * Chooses a random number between low and high, including both.
+     */
+    private static int randomInt(int low, int high) {
+        int range = high - low + 1;
+        return low + random.nextInt(range);
+    }
+
+    /**
+     * Swaps the cards at indexes i and j.
+     */
     private void swapCards(int i, int j){
-        Card temp = this.cards[i];
-        this.cards[i] = this.cards[j];
-        this.cards[j] = temp;
+            Card temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
     }
 
-    //shuffle cards
-    public void shuffle(){
-        for(int i = 0; i<this.cards.length; i++){
-            //choose a random number between the letter i and length-1
-            int rand = randomInt(i, cards.length);
-            //swap the ith card and the randomly chosen card
-            swapCards(i,rand);
+    /**
+     * Sorts the cards (in place) using selection sort.
+     */
+    public void selectionSort() {
+        for (int i=0;i<cards.length-1;i++){
+            swapCards(i, indexLowest(i, cards.length - 1));
         }
     }
 
-    public void selectionSort() {
-       for(int index = 0; index < cards.length; index++){
-           //find the lowest card at or to the right of i
-           int lowest = indexLowest(index, cards.length-1);
-           //swap the ith card and the lowest card found
-           if(index != lowest){
-               swapCards(lowest,index);
-           }
-       }
-    }
-
-    //find the index of the lowest card between low and high inclusive
-    private int indexLowest(int low, int high){
+    /**
+     * Finds the index of the lowest card
+     * between low and high inclusive.
+     */
+    private int indexLowest(int low, int high) {
         int min = low;
-        //where is cards? --> private class variable
-        for(int i = low; i <= high; i++){
-            if(this.cards[i].compareTo(this.cards[min]) < 0) {
+        for (int i = low; i <= high; i++) {
+            if (this.cards[i].compareTo(this.cards[min]) < 0) {
                 min = i;
             }
+
         }
         return min;
-        //find the lowest card between low and high
     }
-    private static Deck merge(Deck deck1, Deck deck2){
-        Deck d3 = new Deck(d1.length+d2.length); //so d3 is big enough for all the cards
 
-        //use the index i to keep track of where we're at in the first deck, and index j is for secodn deck
-        int i = 0;
-        int j = 0;
 
-        for(int k = 0; k < d3.length; k++){
-            //if d1 is empty, use top card from d2
-            //if d1 is empty, use top card from d2
-            //otherwise, compare the top two cards
 
-            //add lowest card to the new deck at k
-            //increment i or j (depending on card)
+    /**
+     * Returns a subset of the cards in the deck.
+     */
+    public Deck subdeck(int low, int high) {
+        Deck sub = new Deck(high - low + 1);
+        for (int i = 0; i < sub.cards.length; i++) {
+            sub.cards[i] = this.cards[low + i];
+        }
+        return sub;
+    }
+
+    /**
+     * Combines two previously sorted subdecks.
+     */
+    private static Deck merge(Deck d1, Deck d2) {
+        Card[] card1 = d1.getCards();
+        Card[] card2 = d2.getCards();
+        int l1 = card1.length;
+        int l2 = card2.length;
+
+        // Create a new array to store the merged cards
+        Card[] sorted = new Card[l1 + l2];
+
+        int i = 0; // Index for c1
+        int j = 0; // Index for c2
+        for (int k = 0; k < sorted.length; k++) {
+            if (j >= card2.length || (i < card1.length && card1[i].compareTo(card2[j]) <= 0)) {
+                sorted[k] = card1[i];
+                i++;
+            } else {
+                sorted[k] = card2[j];
+                j++;
+            }
+        }
+
+        // Create a new deck with the sorted cards
+        Deck deck = new Deck(l1 + l2);
+        System.arraycopy(sorted, 0, deck.cards, 0, sorted.length);
+        return deck;
+    }
+
+    /**
+     * Returns a sorted copy of the deck using selection sort.
+     */
+    public Deck almostMergeSort() {
+        int len = cards.length;
+
+        // if the deck is empty or has only one card, it's already sorted
+        if (len == 0 || len == 1) {
+            return this;
+        }
+
+        // Step 1: Split the deck into two halves
+        int mid = len / 2;
+        Deck d1 = subdeck(0, mid - 1);
+        Deck d2 = subdeck(mid, len - 1);
+
+        // Step 2: Sort each half using selectionSort
+        d1.selectionSort();
+        d2.selectionSort();
+
+        // Step 3: Merge the two sorted halves
+        return merge(d1, d2);
+    }
+    /**
+     * Returns a sorted copy of the deck using merge sort.
+     */
+    public Deck mergeSort() {
+        int len = cards.length;
+        if (len == 0 || len == 1){
+            return this;
+        }
+        else {
+            int mid = len / 2;
+            Deck d1 = subdeck(0, mid - 1).mergeSort();
+            Deck d2 = subdeck(mid, len -1).mergeSort();
+            return merge(d1, d2);
         }
     }
-
-    public Deck almostMergeSort(){
-
-    }
-
-    public Deck mergeSort(){
-        Deck d = new Deck();
-        //if the deck has 0 or 1 cards, return it
-        if(d.length == 0 || d.length==1) return d;
-        //otherwise, divide the deck into two subdecks (subDeck())
-        if(d.length > 1) subDeck();
-        //sort the subdecks using mergesort (mergeSort())
-        //merge the subdecks
-        //return the result
-    }
-
-} //end class deck
+   
+}
